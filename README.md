134d08011c9a158e64dd41148ae2ef2149a613b3
Garrison Mullen <garrisonmullen@yahoo.com>
2023-04-22 15:00:05 +0100

    its all fucky now, am confus

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# django-messenger-attempt
+# django-messenger
 
 I'm trying to make an instant messaging service with Django. Let's see how it goes...
--- im/views.py
+++ im/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import User
 
 # Create your views here.
+def index(request):
+    return render(request, "templates/im/index.html")
+
 def detail(request, user_id):
     question = get_object_or_404(User, pk=user_id)
-    return render(request, "polls/detail.html", {"question": question})
+    return render(request, "templates/im/detail.html", {"question": question})
